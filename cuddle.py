249ebfef1bf31825b0430580557031ea08581311
Catarina607 <69126661+Catarina607@users.noreply.github.com>
2023-04-15 18:46:30 -0300

    Play

---
+++ cuddle.py
@@ -0,0 +1,19 @@
+def owner():
+    import math
+    y = 1
+    while y < 10:
+        w = input('have you finished it? '.lower())
+        if w != 'yes':
+            pi = float(math.pi)
+            for i in range(4, 44, 4):
+                print(pi,'   ',pi)
+                print('          ')
+                print('      ',pi,'   ')
+                print(pi,'   ',pi)
+                print('          ')
+                print('      ',pi,'   ')
+                print(pi,'    ',pi)
+             
+                
+while True:
+    owner()
